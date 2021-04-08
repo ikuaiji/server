426b4b2d5c45b90e62908ecc5346539bd091da23
Athurg Feng <hi@atfeng.com>
2021-04-08 20:18:40 +0800

    完善Bill数据类型的注释

--- bill.go
+++ bill.go
@@ -6,17 +6,18 @@ import (
 	"gorm.io/gorm"
 )
 
+//Bill 代表一次交易账单
 type Bill struct {
 	gorm.Model
 
-	Type       string
-	CategoryId uint
-	AccountId  uint
-	Amount     float32
+	Type       string    //交易类型
+	CategoryId uint      //科目ID
+	AccountId  uint      //交易账户的ID
 	Account2Id uint      //转账交易的对端账户
+	Amount     float32   //金额
 	BillAt     time.Time //入账时间
-	MemberId   uint
-	ProjectId  uint
-	StoreId    uint
-	Note       string
+	MemberId   uint      //成员ID
+	ProjectId  uint      //项目ID
+	StoreId    uint      //商户ID
+	Note       string    //交易备注
 }
