b7d230ba33e2175d4095cb8468c4b562a12ea881
Athurg Feng <hi@atfeng.com>
2021-04-21 17:50:54 +0800

    增加静态文件处理机制，支持代理前端请求

--- cmd/webserver/main.go
+++ cmd/webserver/main.go
@@ -4,17 +4,20 @@ import (
 	"app/db"
 	"flag"
 	"log"
+	"os"
+	"path"
 
 	"github.com/gin-gonic/gin"
 )
 
-var dsn, listen string
+var dsn, listen, staticPath string
 var ginEngine = gin.Default()
 var apiRouter = ginEngine.Group("/api")
 
 func init() {
 	flag.StringVar(&dsn, "dsn", "root:root@tcp(127.0.0.1:3306)/laokuaiji", "数据库DSN")
 	flag.StringVar(&listen, "listen", ":8081", "侦听地址")
+	flag.StringVar(&staticPath, "static", "../web/dist", "前端静态文件的路径")
 }
 
 func main() {
@@ -26,6 +29,25 @@ func main() {
 		log.Fatal(err)
 	}
 
+	ginEngine.NoRoute(StaticFileHandler)
+
 	//启动HTTP侦听器
 	ginEngine.Run(listen)
+}
+
+//StaticFileHandler 用于处理前端提供的静态文件
+func StaticFileHandler(c *gin.Context) {
+	//判断请求路径是否是文件，如果是则返回
+	file := path.Join(staticPath, c.Request.URL.Path)
+	if fi, _ := os.Stat(file); fi != nil && fi.Mode().IsRegular() {
+		c.File(file)
+		return
+	}
+
+	file = path.Join(file, "index.html")
+	//如果不是，则尝试改路径下的index.html文件，如果是则返回
+	//如果不是，按缺省设置处理（是文件夹则返回文件列表、其他返回404）
+	if fi, _ := os.Stat(file); fi != nil && fi.Mode().IsRegular() {
+		c.File(file)
+	}
 }
